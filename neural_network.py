d3f653ee3ab0a97e0af017d3e31c80e3fff96fa8
Stefan Miljevic <stefi5miljevic@gmail.com>
2020-12-31 19:54:32 +0100

    init commit

---
+++ neural_network.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class NeuralNetwork:
+    # TODO
+    def __init__(self):
+        return
+
+    def train(self):
+        return
+
+    def predict(self):
+        return
+
+    def forward_prop(self):
+        return
+
+    def backward_prop(self):
+        return
